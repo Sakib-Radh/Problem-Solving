c9257a7be9e8175d154ab02382c2fde4c262da19
Sakib-Radh <62024635+Sakib-Radh@users.noreply.github.com>
2021-10-07 22:01:43 +0600

    learning map

---
+++ learning_map.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+
+    string s = "amikicukhaina";
+    map<char, int>m;
+
+    for(auto u : s)
+        {m[u]++;}
+
+    for(auto u : m)
+        P{cout<<u.first()<<" "<<u.second();}
+
+    }
