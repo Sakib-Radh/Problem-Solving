0ac961acd8ccc8a1da50d8eba3912bfd22ac91ea
Sakib-Radh <62024635+Sakib-Radh@users.noreply.github.com>
2021-10-12 13:55:46 +0600

    Create map_count.cpp

---
+++ map_count.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[10] = {1, 0, 0, 0, 1, 0,1 ,1};
+    int i;
+    map<int, int>freq;
+
+    for(i =0 ; i<8; i++)
+    {
+        freq[a[i]]++;
+
+    }
+
+
+
+}
