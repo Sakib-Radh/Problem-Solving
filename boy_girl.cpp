48ad41e9ff26a2e50bc03e96c722430c0d0dd370
Sakib-Radh <62024635+Sakib-Radh@users.noreply.github.com>
2021-11-12 02:08:05 +0600

    codeforces problem solved

--- boy_girl.cpp
+++ boy_girl.cpp
@@ -1,31 +1,49 @@
-
-#include<iostream>
-#include<string>
 #include<bits/stdc++.h>
+#include<map>
 using namespace std;
 
 int main(){
 
-    string s;
-    int i, j, l, c=0;
+    map<char, int>m;
+    string s, a;
+    //char temp;
+    int all = 0;
+
+    int i, j, l;
 
     cin>>s;
 
+    //a[0] = s[0];
+
     l = s.length();
 
-    for(i=0; i<l-1; i++)
+    for(i=0; i<l; i++)
     {
-        for(j=i+1; j<l; j++)
-        {
-            if(s[i] == s[j])
-
-            {
-               s.erase(j);
-               break;
-
-            }
-        }
+        m[s[i]]++;
     }
+
+    map<char, int>::iterator itr;
+
+    for(itr = m.begin(); itr != m.end(); ++itr)
+    {
+        //cout<<itr->first<<endl;
+        all++;
+    }
+
+    //cout<<all<<endl;
+
+    if(all%2 == 0)
+    {
+        cout<<"CHAT WITH HER!"<<endl;
+    }
+
+    else
+        cout<<"IGNORE HIM!"<<endl;
+
+    //cout<<a<<endl;
+
+
+
 
     //cout<<s<<endl;
     //cout<<c<<endl;
--- string_task.cpp
+++ string_task.cpp
@@ -1,27 +1,37 @@
-#include<iostream>
-#include<string>
+#include<bits/stdc++.h>
+
 using namespace std;
-int main(){
 
-string s, b;
-int i;
-cin>>s;
-
-int a;
-
-a=s.length();
-
-for(i=0;i<a;i++)
+int main()
 {
-   if(s[i]='a'||'e'||'i'||'o'||'u')
-        b[i] = s.erase(i);
-}
+    string s, a;
+    cin>>s;
+    //a[0] = s[0];
 
-cout<<b;
+    int l, i, j;
+
+    l = s.length();
+
+    for(i=0; i<l; i++)
+    {
+        if(s[i] >= 65 and s[i] <= 90)
+        {
+            s[i] = s[i] + 32;
+        }
+    }
 
 
+    //cout<<a[i]<<endl;
 
+for(i=0; i<l; i++)
+    {
 
+        if(s[i] != 'a' and s[i] != 'e' and s[i] != 'i' and s[i] != 'o' and s[i] != 'u')
+        {
+            cout<<"."<<s[i];
+        }
 
+    }
 
+    //cout<<a<<endl;
 }
